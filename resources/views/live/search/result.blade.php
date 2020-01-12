9b57e20ab583bde71b20fe54c33edab21ac341ba
YutaKanna <kannayuta0629@gmail.com>
2020-01-12 20:25:21 +0900

    検索結果を表示する #8

--- resources/views/live/search/result.blade.php
+++ resources/views/live/search/result.blade.php
@@ -2,8 +2,8 @@
 @section('content')
     <div class="container">
         <div class="row">
-            @foreach ($videoIds as $videoId)
-                <iframe class="mt-1" width="368" height="225" src="https://www.youtube.com/embed/{{ $videoId }}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            @foreach ($videoIds as $key => $videoId)
+                <iframe class="mt-1" width="368" height="225" src="https://www.youtube.com/embed/{{ $videoIds[$key]['videoId'] }}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             @endforeach
         </div>
     </div>
