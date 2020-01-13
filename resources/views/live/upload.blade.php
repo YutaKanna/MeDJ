bcd5bbd4160530e9b6901277eba5c05470593bf1
YutaKanna <kannayuta0629@gmail.com>
2020-01-13 18:57:38 +0900

    自動停止を消す #12

--- resources/views/live/upload.blade.php
+++ resources/views/live/upload.blade.php
@@ -29,7 +29,6 @@ event.target.playVideo();
 var done = false;
 function onPlayerStateChange(event) {console.log(3);
 if (event.data == YT.PlayerState.PLAYING && !done) {
-  setTimeout(stopVideo, 2000);
   done = true;
 }
 }
@@ -46,16 +45,15 @@ player.stopVideo();
         <div class="row justify-content-center">
             <div class="col">
                 <div class="mt-4 mx-auto">
-                    <i class="fas fa-cut fa-5x" style="color: #55acee;"></i>
+                    <i id="upload" class="fas fa-cut fa-5x" style="color: #55acee;"></i>
                     <iframe id="player" width="560" pause="810" height="315" src="https://www.youtube.com/embed/{{ $videoId }}?rel=0&fs=0&modestbranding=1&enablejsapi=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    <button id="hoge">再生</button>
                 </div>
             </div>
         </div>
     </div>
 
     <script>
-        var ytPause = document.getElementById('hoge');
+        var ytPause = document.getElementById('upload');
         ytPause.addEventListener('click', function() {
             player.pauseVideo();
         });
