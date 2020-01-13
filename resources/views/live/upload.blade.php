55da8901cf1c508275f21db611a83dba3853da61
YutaKanna <kannayuta0629@gmail.com>
2020-01-13 18:52:59 +0900

    ハサミをクリックした時に動画を停止できるようにする  #12

--- resources/views/live/upload.blade.php
+++ resources/views/live/upload.blade.php
@@ -2,6 +2,42 @@
 
 @section('head')
 <script type="text/javascript">
+
+//api用のJSを読み込む
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+//APIを実行
+ var player;
+  function onYouTubeIframeAPIReady() {
+	player = new YT.Player("player",{
+	events: {
+        'onReady': onPlayerReady,//API呼び出しの受信を開始する準備ができると起動
+        'onStateChange': onPlayerStateChange// プレーヤーの状態が変わると起動
+      }
+	});
+  }
+
+//関数
+function onPlayerReady(event) {
+event.target.playVideo();
+}
+
+//関数（以下は再生後2秒で止まる）
+var done = false;
+function onPlayerStateChange(event) {console.log(3);
+if (event.data == YT.PlayerState.PLAYING && !done) {
+  setTimeout(stopVideo, 2000);
+  done = true;
+}
+}
+
+function stopVideo() {
+player.stopVideo();
+}
+
 </script>
 @endsection
 
@@ -11,9 +47,17 @@
             <div class="col">
                 <div class="mt-4 mx-auto">
                     <i class="fas fa-cut fa-5x" style="color: #55acee;"></i>
-                    <iframe width="560" pause="810" height="315" src="https://www.youtube.com/embed/{{ $videoId }}?rel=0&fs=0&modestbranding=1&enablejsapi=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe id="player" width="560" pause="810" height="315" src="https://www.youtube.com/embed/{{ $videoId }}?rel=0&fs=0&modestbranding=1&enablejsapi=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <button id="hoge">再生</button>
                 </div>
             </div>
         </div>
     </div>
+
+    <script>
+        var ytPause = document.getElementById('hoge');
+        ytPause.addEventListener('click', function() {
+            player.pauseVideo();
+        });
+    </script>
 @endsection
