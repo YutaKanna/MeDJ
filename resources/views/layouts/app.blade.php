2acb5e2d407fa05e843dd9e1248d6439c9a5f63d
YutaKanna <kannayuta0629@gmail.com>
2020-01-19 23:35:04 +0900

    投稿するリンクを常にヘッダーに配置(未ログインならログイン画面に遷移) #28

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,10 +47,10 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('live.to_input') }}">{{ __('投稿する') }}</a>
+                        </li>
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('live.to_input') }}">{{ __('投稿する') }}</a>
-                            </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
